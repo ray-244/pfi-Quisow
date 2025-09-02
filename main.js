e88fb0b412d9816da871c4daf0195c24c005d471
ray-244 <keren.rodrigues@escola.pr.gov.br>
2025-09-02 14:01:35 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+// Basic script file for future interactions
+console.log("QuiShow Home Page Loaded");
+
+// Example of adding a simple interactive element (optional for now)
+// document.querySelectorAll('.card').forEach(card => {
+//     card.addEventListener('click', () => {
+//         console.log('Card clicked!');
+//         // Add more complex interaction here later
+//     });
+// });
